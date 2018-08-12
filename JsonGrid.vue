5bf34073735b9258a806ede405ed465db189d4f3
jf5577 <jfempire@yahoo.co.uk>
2018-08-12 18:43:49 +0530

    firt release

---
+++ JsonGrid.vue
@@ -0,0 +1,84 @@
+// @name JsonGrid Component 
+// @version 20180812
+// @author Julian Frank
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="(name,i) in dispData.columns" :key="i" @click="titleClicked(name)">{{name}}</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <td v-for="(name,i) in dispData.columns" :key="i">{{name}}</td>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr v-for="(row,i) in dispData.rows" :key="i">
+                    <td v-for="(data,i) in row" :key="i">{{data}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+  props: ["data", "search"],
+  data: () => {
+    return {
+      sortcolumn: "datetime",
+      sorttype: -1
+    };
+  },
+  methods: {
+    titleClicked(name) {
+      if (this.sortcolumn == name) {
+        this.sorttype *= -1;
+      } else {
+        this.sortcolumn = name;
+      }
+    }
+  },
+  computed: {
+    dispData() {
+      let columns = [];
+      this.data.forEach(el => {
+        for (const key in el) {
+          if (el.hasOwnProperty(key)) {
+            if (columns.indexOf(key) < 0) columns.push(key);
+          }
+        }
+      });
+      let rows = [];
+      this.data.forEach(el => {
+        let row = columns.map((val, ind) => {
+          if (el[val]) {
+            return JSON.stringify(el[val], "", "\t");
+          } else {
+            return "-";
+          }
+        });
+        if (this.search.length > 0) {
+          let rowStr = row.join(" ").toLowerCase();
+          if (rowStr.indexOf(this.search.toLowerCase()) >= 0) rows.push(row);
+        } else {
+          rows.push(row);
+        }
+      });
+      if (this.sortcolumn) {
+        //[WIP] Make this more sophisticated with data type discovery
+        let ind = columns.indexOf(this.sortcolumn);
+        rows.sort((a, b) => {
+          if (a[ind] > b[ind]) {
+            return 1 * this.sorttype;
+          } else {
+            return -1 * this.sorttype;
+          }
+        });
+      }
+      return { columns: columns, rows: rows };
+    }
+  }
+};
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # JsonGrid
+
 Vue Component that takes your JSON and displays as a HTML Table with superpowers
+
+## Usage
+
+### Using Bundlers
+
+When using bundlers like ParcelJS copy the JsonGrid.vue file to your components folder. Then in your script import and then use like any other component
+
+```html
+<template>
+  <div>
+...
+      <JsonGrid :data="dataJSON" :search="searchQuery"></JsonGrid>
+...
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import JsonGrid from "./components/JsonGrid";
+
+export default {
+    components: {JsonGrid},
+    data:()=>{return {
+        dataJSON:{"a","A","b":"B"},
+        searchQuery:"a"
+    }}
+}
+</script>
+```
+
+### Directly using VanillaJS
+
+For direct JS useage ensure that VueJS is loaded and then followed by the JsonGrid.js file. Then in your script use like any other component. Use the JsonGrid Tag in HTML
+
+```html
+<html>
+  <div>
+...
+      <JsonGrid :data="dataJSON" :search="searchQuery"></JsonGrid>
+...
+  </div>
+</html>
+
+import Vue from 'vue'
+import JsonGrid from "./components/JsonGrid";
+
+new Vue({
+    el: '#myApp',
+    components: {JsonGrid},
+    data:()=>{return {
+        dataJSON:{"a","A","b":"B"},
+        searchQuery:"a"
+    }}
+})
+```
+
+## Features
+
+1. Automatically detects the column heads from the Keys in the JSON provided
+1. Column Sorting on clicking the Column headings
+1. Search Data used to filter the rows with the search term used
+
+## Future Features
+
+1. Sorting using more smart detection of data of the column selected
+1. Table Format and Styling Capability
